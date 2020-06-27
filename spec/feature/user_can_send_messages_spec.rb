b52ec7416e2645b27a7354bb0104c32215c6c0d5
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-06-27 18:56:26 +0200

    added Warden in rspec config to help write test for send message and receive message. Rewrote test for sending and receiving messages all test pass

--- spec/feature/user_can_send_messages_spec.rb
+++ spec/feature/user_can_send_messages_spec.rb
@@ -1,36 +1,24 @@
-require 'rails_helper'
+feature "user can send messages" do 
+    let(:sender) {create(:user, email: 'user@mail.com', password:'12345678', name:'Name') }
+    let!(:receiver) {create(:user, email: 'bigrat@mail.com',password:'11223344', name:'Bigrat') }
 
-feature 'user can send messages' do
-    context 'sending messages' 
+    describe "sender logs-in and sends message" do    
+        before do
+            login_as(sender, scope: :user)
+            visit new_conversation_path
+            select "Bigrat", from: "Recipients"
+            fill_in "Subject", with: "Hej"
+            fill_in "Type your message here", with: "Hej då"
+            click_on "Send Message"  
+        end
 
-    before do
-      visit user_session_path
-      create(:user, email: 'user@mail.com', password: '12345678', name: 'Name')
-      create(:user, email: 'bigrat@mail.com', password: '11223344', name: 'bigrat')
-      click_on 'Login'
-      fill_in 'Email', :with => 'user@mail.com'
-      fill_in 'Password', :with => '12345678'
-      click_on 'Log in'
+        it "receives success message" do 
+            expect(page).to have_content "Your message was successfully sent!"
+        end
+
+        it "message is visible in the sent messages section" do 
+            visit mailbox_sent_path
+            expect(page).to have_content "Hej då"
+        end
     end
-
-
-    it 'check if there is an inbox button' do
-      expect(page).to have_content'Inbox'
-    end
-
-    it 'click on inbox to compose message, select recipient and send' do
-      click_on 'Inbox'
-      expect(page).to have_content 'Compose'
-      click_on 'Compose'
-      select 'bigrat', :from => 'Recipients'
-      fill_in 'Subject', :with => 'hey'
-      fill_in 'Type your message here', :with => 'hej då'
-      click_on 'Send Message'
-      expect(page).to have_content 'Your message was successfully sent!'
-    end
-
-  
-
-    
-
 end
